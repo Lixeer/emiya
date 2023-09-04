5b7e9faa064188e35c8a230272eb8c92bff1fbfc
Spanx <89501533+Lixeer@users.noreply.github.com>
2023-09-04 21:07:24 +0800

    Update Logger.py
    更换命名规范 单例惰性实现log 提供函数访问

--- libs/Logger.py
+++ libs/Logger.py
@@ -20,3 +20,34 @@ class Log(Singleton):
     def log_debug(self, message):
         now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         print(f"\033[34m[{now}] [DEBUG] {message}\033[0m")
+
+
+
+
+
+
+    
+    def logInfo(self, message):
+        now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        print(f"\033[32m[{now}] [INFO] {message}\033[0m")
+
+    def logWarning(self, message):
+        now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        print(f"\033[33m[{now}] [WARNING] {message}\033[0m")
+
+    def logError(self, message):
+        now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        print(f"\033[31m[{now}] [ERROR] {message}\033[0m")
+
+    def logDebug(self, message):
+        now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        print(f"\033[34m[{now}] [DEBUG] {message}\033[0m")
+
+
+
+#单例Log惰性实现
+privatelog=Log()
+logInfo=privatelog.logInfo
+logWarning=private.logWarning
+logError=private.logWarning
+logDebug=private.logDebug
