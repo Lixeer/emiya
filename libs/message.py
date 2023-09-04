e53c271d15fad5eca5809af7b23ac30832718dbf
wzh <wzh_yjy@163.com>
2023-09-04 17:35:13 +0800

    第一次更新

---
+++ libs/message.py
@@ -0,0 +1,33 @@
+from libs.Logger import Log
+from libs.singleton import Singleton
+
+log = Log()
+
+"""
+定义消息类(上报输出)
+"""
+
+
+class MessageInput(Singleton):
+    def info(self, data: dict):  # debug时使用，输出全部data
+        if data['post_type'] != 'meta_event':
+            log.log_info(data)
+
+    def info_group(self, data: dict):  # group
+        log.log_info(f'{data["message_type"]} | {data["sender"]["nickname"]}:{data["message"]}')
+
+    def info_private(self, data: dict):  # private
+        log.log_info(f'{data["message_type"]} | {data["sender"]["nickname"]}:{data["message"]}')
+
+    def choose(self, data: dict):  # 进行消息分流
+        try:
+            if data['meta_event_type'] == 'heartbeat':
+                pass
+        except:
+            if data['message_type'] == 'private':
+                self.info_private(data)
+            if data['message_type'] == 'group':
+                self.info_group(data)
+
+    def cheak(self, data: dict):  # data数据传入这个会自动分流
+        self.choose(data)
--- main.py
+++ main.py
@@ -1,50 +1,38 @@
-#coding:gbk
+# coding:gbk
 
-from fastapi import FastAPI, Request
 import requests
+from fastapi import Request, FastAPI
+
+from libs.Logger import Log
+from libs.message import MessageInput
 
 BASEURL = 'http://127.0.0.1:5700'
-# r = requests.get(url='http://127.0.0.1:5700/get_login_info')
-# print(r.json())
 
 
 def sendGroupMsg(gid: int, text: str):
-    # print('进入sendGroupMsg')
     d = {'message': text,
          'group_id': gid}
     print(requests.post(f'{BASEURL}/send_group_msg', data=d))
+
 
 def getMsg(id: int):
     d = {'message_id': id}
     return requests.post(f'{BASEURL}/get_msg', data=d)
 
+
 app = FastAPI()
+log = Log()
+msg = MessageInput()
+
 
 @app.post('/')
 async def handle(request: Request):
-    try:
-        data = await request.json()
-        # print('打印输出data：', data, '\n', '='*40)
-        if data['post_type'] == 'meta_event':
-            return 'a'
-        msgid = data['message_id']
-        # print('打印输出msgid=', msgid, '\n', '='*40)
+    msg.cheak(await request.json())
+    return "data"
 
-        msg = getMsg(msgid).json()
-        # print(msg)
-        print(msg['data']['sender']['nickname'], (msg['data']['sender']['user_id']), '：', msg['data']['message'])
-        msgtext = msg['data']['message']
-        # print('打印输出msg=', msg, '\n', '='*40)
-        if msgtext == '原神':
-            # print('进入’原神‘if', '\n', '='*40)
-            gid = msg['data']['group_id']
-            # print(f'打印输出gid：{gid}', '\n', '='*40)
-            sendGroupMsg(gid=gid, text='启动你麻痹')
-        return 'ts'
-    except Exception as ec:
-        print(f'报错信息：{ec}')
+
 if __name__ == '__main__':
+    log.log_info("emiya�������")
     import uvicorn
-    uvicorn.run(app, port=5701, host='0.0.0.0')
 
-
+    uvicorn.run(app, port=5701, host='0.0.0.0', log_level="warning")
