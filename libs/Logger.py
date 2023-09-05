f808ce469ec979f4b31c37da15e8a0d328819140
元英 <130777751+MrWhite416@users.noreply.github.com>
2023-09-05 17:25:10 +0800

    Update Logger.py

--- libs/Logger.py
+++ libs/Logger.py
@@ -1,3 +1,4 @@
+
 import threading
 from datetime import datetime
 
@@ -6,22 +7,6 @@ from libs.singleton import Singleton
 
 class Log(Singleton):
     # 可以是非单例 但是要有全局访问节点 代表状态的属性可以放静态变量里面
-    def log_info(self, message):
-        now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        print(f"\033[32m[{now}] [INFO] {message}\033[0m")
-
-    def log_warning(self, message):
-        now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        print(f"\033[33m[{now}] [WARNING] {message}\033[0m")
-
-    def log_error(self, message):
-        now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        print(f"\033[31m[{now}] [ERROR] {message}\033[0m")
-
-    def log_debug(self, message):
-        now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        print(f"\033[34m[{now}] [DEBUG] {message}\033[0m")
-
     def logInfo(self, message):
         now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         print(f"\033[32m[{now}] [INFO] {message}\033[0m")
@@ -31,6 +16,21 @@ class Log(Singleton):
         print(f"\033[33m[{now}] [WARNING] {message}\033[0m")
 
     def logError(self, message):
+        now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        print(f"\033[31m[{now}] [ERROR] {message}\033[0m")
+
+    def logDebug(self, message):
+        now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        print(f"\033[34m[{now}] [DEBUG] {message}\033[0m")
+
+
+# 单例Log惰性实现
+privatelog = Log()
+logInfo = privatelog.logInfo
+logWarning = privatelog.logWarning
+logError = privatelog.logWarning
+logDebug = privatelog.logDebug
+
         now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         print(f"\033[31m[{now}] [ERROR] {message}\033[0m")
 
