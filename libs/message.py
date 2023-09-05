1c55e7a49d43e7d8129896059185e59e06d518a4
元英 <130777751+MrWhite416@users.noreply.github.com>
2023-09-05 17:26:09 +0800

    Update message.py

--- libs/message.py
+++ libs/message.py
@@ -1,8 +1,9 @@
+
 from libs.Logger import Log
 from libs.singleton import Singleton
 
 
-
+#要改 后面的输出也要改成小写驼峰而不是大驼峰 改完删掉这行主食
 log = Log()
 
 """
@@ -13,13 +14,13 @@ log = Log()
 class MessageInput(Singleton):
     def info(self, data: dict):  # debug时使用，输出全部data
         if data['post_type'] != 'meta_event':
-            log.log_info(data)
+            log.logInfo(data)
 
-    def info_group(self, data: dict):  # group
-        log.log_info(f'{data["message_type"]} | {data["sender"]["nickname"]}:{data["message"]}')
+    def infoGroup(self, data: dict):  # group
+        log.logInfo(f'{data["message_type"]} | {data["sender"]["nickname"]}:{data["message"]}')
 
-    def info_private(self, data: dict):  # private
-        log.log_info(f'{data["message_type"]} | {data["sender"]["nickname"]}:{data["message"]}')
+    def infoPrivate(self, data: dict):  # private
+        log.logInfo(f'{data["message_type"]} | {data["sender"]["nickname"]}:{data["message"]}')
 
     def choose(self, data: dict):  # 进行消息分流
         try:
@@ -27,9 +28,9 @@ class MessageInput(Singleton):
                 pass
         except:
             if data['message_type'] == 'private':
-                self.info_private(data)
-            if data['message_type'] == 'group':
-                self.info_group(data)
+                self.infoPrivate(data)
+            elif data['message_type'] == 'group':
+                self.infoGroup(data)
 
     def cheak(self, data: dict):  # data数据传入这个会自动分流
         self.choose(data)
