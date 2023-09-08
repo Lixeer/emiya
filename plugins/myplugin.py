ffdf651c05b14eee4b95d8298ca8e2e61cc31425
robo_head <2364082093@qq.com>
2023-09-09 03:04:40 +0800

    update ignored

--- plugins/myplugin.py
+++ plugins/myplugin.py
@@ -8,7 +8,6 @@ msg = Message()
 normal = " (going well)"
 
 if __name__ == "__main__":
-    # msg.send_private_msg(2364082093, "send_private_msg"+normal)
     # msg.send_group_msg(746490988, "send_group_msg"+normal)
     res = msg.get_friend_list(True)
     data = res['data']
