50722ebfff592f4796f09facfcba206f909f30f0
元英 <130777751+MrWhite416@users.noreply.github.com>
2023-09-12 16:09:35 +0800

    Add files via upload

--- libs/cqinit.py
+++ libs/cqinit.py
@@ -3,21 +3,18 @@ import os
 from abc import ABC,abstractmethod
 
 
-
-
-
 class InitWay:
 	registeredWay=[]
 	def __init__(self):
 		pass
 	@abstractmethod
-	def run()->None:
+	def run(self)->None:
 		pass
 		
 		
 		
 	@classmethod	
-	def start():
+	def start(self):
 		for e in self.registeredWay:
 			e.run()
 
@@ -30,11 +27,12 @@ def register(clas):
 
 @register
 class BatWay(InitWay):
-	def run():
-		os.system("./go-cqhttp.bat")
-	
-class BatWay(InitWay):
+	def run(self):
+		os.system(".\go-cqhttp.bat")
+
+class PWay(InitWay):
 	#补全管道启动方式(策略)
 	pass
 
 init=InitWay.start
+print('kkk')
