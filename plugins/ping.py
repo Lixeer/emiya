27067da16edf088a70e0ccc303c6ca8607d6fce3
Spanx <89501533+Lixeer@users.noreply.github.com>
2023-09-29 14:19:44 +0800

    Add files via upload

--- plugins/ping.py
+++ plugins/ping.py
@@ -1,7 +1,32 @@
 # coding:utf-8
 
-from libs.evvent,qqevent import onkeyword
+from libs.event.qqevent import onkeyword
+import requests
 
-@onkeyword(keywordList=["ping"]):
+BASEURL = "http://127.0.0.1:5700"
+def sendGroupMsg(gid: int, text: str):
+    d = {"message": text, "group_id": gid}
+    print(requests.post(f"{BASEURL}/send_group_msg", data=d))
+
+
+
+
+
+             ###############         ################
+             # 快快写action!#         # 我有抑郁症.jpg #
+             ###############         ################
+
+
+
+
+@onkeyword(keywordList=["ping"])
 def handle(netpackage):
+
+    try:
+        sendGroupMsg(gid=netpackage.group_id,text="pong")
+    except:
+        pass
+
     print("hello emiya")
+
+
