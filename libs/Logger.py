c532c4d75c38eef00cf84db3e430d2a257f3cc28
Spanx <89501533+Lixeer@users.noreply.github.com>
2023-10-03 16:21:27 +0800

    Update Logger.py

--- libs/Logger.py
+++ libs/Logger.py
@@ -9,19 +9,19 @@ class Log(Singleton):
     # 可以是非单例 但是要有全局访问节点 代表状态的属性可以放静态变量里面
     def logInfo(self, message):
         now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        print(f"\033[32m[{now}] [INFO] {message}\033[0m")
+        print(f"[{now}] [INFO] {message}")
 
     def logWarning(self, message):
         now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        print(f"\033[33m[{now}] [WARNING] {message}\033[0m")
+        print(f"[{now}] [WARNING] {message}")
 
     def logError(self, message):
         now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        print(f"\033[31m[{now}] [ERROR] {message}\033[0m")
+        print(f"[{now}] [ERROR] {message}")
 
     def logDebug(self, message):
         now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        print(f"\033[34m[{now}] [DEBUG] {message}\033[0m")
+        print(f"[{now}] [DEBUG] {message}")
 
 
 # 单例Log惰性实现
