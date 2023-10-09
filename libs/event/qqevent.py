ec301bece8c049ed4378d2141da81a29314ac6f4
Spanx <89501533+Lixeer@users.noreply.github.com>
2023-10-09 16:55:19 +0800

    Update qqevent.py

--- libs/event/qqevent.py
+++ libs/event/qqevent.py
@@ -110,7 +110,6 @@ def onkeyword(keywordList, rate=1):
     def rg(callback):
         EventControl().eventList.append((KeyWordEvent(rate=rate, keywordList=keywordList), callback))
         # print("38",EventControl().eventList)
-
     return rg
 
 
@@ -118,11 +117,16 @@ def oncommand(*, promat: list[str], cmd: list[str], rate=1):
     def rg(callback):
         EventControl().eventList.append((CommandEvent(cmd=cmd, prompt=promat), callback))
         # print("38",EventControl().eventList)
-
     return rg
+
+
 def onmessage(rate=1):
     def rg(callback):
         EventControl().eventList.append((MessageEvent(),callback))
+    
+    return rg
+
+
 def ontest(number, rate=1):
     def rg(callback):
         EventControl().eventList.append((TestEvent(number=number),callback))
