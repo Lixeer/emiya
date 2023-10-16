22c7d0c4e1e3da940f8e907606c0f15df3436fd4
chen3a15 <chen3a15@163.com>
2023-10-16 15:43:57 +0800

    日志

--- libs/Logger.py
+++ libs/Logger.py
@@ -1,9 +1,9 @@
 
-import threading
 from datetime import datetime
 
 from libs.singleton import Singleton
 
+import threading
 import os
 import sys
 
@@ -15,15 +15,13 @@ def getCall():
     fileName = sys._getframe(2).f_code.co_filename
   except:
     return "?"
-  callName = ""
-  fileNameList = fileName.replace(os.getcwd(), "").replace("\\", "/").lstrip("/").split("/")
-  if fileNameList[-1][-3:] == ".py":
-    fileNameList[-1] = fileNameList[-1][0:-3]
-  for i in fileNameList:
-    callName = callName + "." + i
+  callName = fileName.replace(os.getcwd(), "").replace("\\", "/").replace("/", ".").lstrip("/")
+  if callName[-3:] == ".py":
+    callName = callName[0:-3]
   if callName[-8:] == "__init__":
     callName = callName[0:-9]
-  callName = callName[1:]
+  if callName[0:1] == ".":
+    callName = callName[1:]
   return callName
 
 class Log(Singleton):
@@ -31,28 +29,27 @@ class Log(Singleton):
     def logInfo(self, message):
         now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         call = getCall()
-        print(f"[\033[40;36m{now}\033[0m] [\033[40;32mINFO\033[0m] \033[40;36;4m{call}\033[0m \033[40;32m{message}\033[0m")
+        print(f"[\033[40;36m{now}\033[0m] [\033[40;32mINFO\033[0m] \033[40;36;4m{call}\033[0m | \033[40;32m{message}\033[0m")
 
     def logWarning(self, message):
         now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         call = getCall()
-        print(f"[\033[40;36m{now}\033[0m] [\033[40;33mWARNING\033[0m] \033[40;36;4m{call}\033[0m \033[40;33m{message}\033[0m")
+        print(f"[\033[40;36m{now}\033[0m] [\033[40;33mWARNING\033[0m] \033[40;36;4m{call}\033[0m | \033[40;33m{message}\033[0m")
 
     def logError(self, message):
         now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         call = getCall()
-        print(f"[\033[40;36m{now}\033[0m] [\033[40;31mERROR\033[0m] \033[40;36;4m{call}\033[0m \033[40;31m{message}\033[0m")
+        print(f"[\033[40;36m{now}\033[0m] [\033[40;31mERROR\033[0m] \033[40;36;4m{call}\033[0m | \033[40;31m{message}\033[0m")
 
     def logDebug(self, message):
         now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         call = getCall()
-        print(f"[\033[40;36m{now}\033[0m] [\033[40;37mDEBUG\033[0m] \033[40;36;4m{call}\033[0m  \033[40;37m{message}\033[0m")
+        print(f"[\033[40;36m{now}\033[0m] [\033[40;37mDEBUG\033[0m] \033[40;36;4m{call}\033[0m | \033[40;37m{message}\033[0m")
 
 
 # 单例Log惰性实现
 privatelog = Log()
 logInfo = privatelog.logInfo
 logWarning = privatelog.logWarning
-logError = privatelog.logWarning
+logError = privatelog.logError
 logDebug = privatelog.logDebug
-
