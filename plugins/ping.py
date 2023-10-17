2dd469b07d1b21d499b73d07fb3d42da51afe01a
Spanx <89501533+Lixeer@users.noreply.github.com>
2023-10-17 21:40:52 +0800

    Update ping.py

--- plugins/ping.py
+++ plugins/ping.py
@@ -3,13 +3,6 @@
 from libs.event.qqevent import onkeyword,oncommand
 from libs.netpackage.postnetpackage import MessagePackage
 
-import requests
-
-BASEURL = "http://127.0.0.1:5700"
-def sendGroupMsg(gid: int, text: str):
-    d = {"message": text, "group_id": gid}
-    requests.post(f"{BASEURL}/send_group_msg", data=d)
-
 
 
 
@@ -22,26 +15,21 @@ def sendGroupMsg(gid: int, text: str):
 
 
 @onkeyword(keywordList=["ping"])
-def handle(netpackage:MessagePackage):
+async def handle(netpackage:MessagePackage):
+    id=netpackage.getID()
+    d={
+        "group_id":id,
+        "h":"pong"
+    }
+    
+    
 
-    try:
-        sendGroupMsg(gid=netpackage.group_id,text="pong")
-    except:
-        pass
+    
 
-    print("hello emiya")
+    
 
 
 
-
-@oncommand( promat = [".","/"] , cmd = ["ts"])
-def handle(netpackage:MessagePackage):
-    print("command触发")
-
-    try:
-        sendGroupMsg(gid=netpackage.group_id,text=f"参数为{netpackage.arg}")
-    except:
-        pass
 
 
 
