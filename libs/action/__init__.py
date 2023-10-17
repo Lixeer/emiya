b4e3560c0cdb1006153f14b9ed259117197d66d3
Spanx <89501533+Lixeer@users.noreply.github.com>
2023-10-17 21:15:58 +0800

    实现action

--- libs/action/__init__.py
+++ libs/action/__init__.py
@@ -1,2 +1,16 @@
 #coding:utf-8
+
 from fastapi import WebSocket
+
+class Action:
+  def __init__(self,websocket:WebSocket):
+    self.websocket=websocket
+    self._sendText=websocket.send_text
+  async def callApi(url,parmams):
+    d={"action":url,
+       "params":params
+      }
+    response = await self.sendText(d)
+    return response
+   
+
