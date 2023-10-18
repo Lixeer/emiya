087eaccce914e62485e9e91f0c9e6f6da2c0adff
Spanx <89501533+Lixeer@users.noreply.github.com>
2023-10-18 20:02:53 +0800

    更新 ping.py

--- plugins/ping.py
+++ plugins/ping.py
@@ -7,17 +7,14 @@ from libs.netpackage.postnetpackage import MessagePackage
 
 
 
-             ###############         ################
-             # 快快写action!#         # 我有抑郁症.jpg #
-             ###############         ################
-
+    
 
 
 
 @onkeyword(keywordList=["ping"])
 async def handle(netpackage:MessagePackage):
     id=netpackage.getID()
-    rp=await netpackage.actioner.callApi(url="send_group_message",group_id=id,message="pong")
+    rp=await netpackage.actioner.callAPI(url="send_group_message",group_id=id,message="pong")
   
     
     
