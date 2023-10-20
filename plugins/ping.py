fe81c78671158c83e48e887aef199153d557efc0
Hananemu <147488578+Hananemu@users.noreply.github.com>
2023-10-20 15:21:54 +0800

    Add files via upload

--- plugins/ping.py
+++ plugins/ping.py
@@ -14,7 +14,7 @@ from libs.event.qqevent import onkeyword,oncommand
 @onkeyword(keywordList=["ping"])
 async def handle(n):
     id=n.netpackage.getID()
-    rp=await n.callAPI(url="send_group_message",group_id=id,message="pong")
+    rp=await n.callAPI(url="send_group_msg",group_id=id,message="pong")
   
     
     
