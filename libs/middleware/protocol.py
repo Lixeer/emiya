dc397a90e3ee8c659f5a70cc1afd7b622966eebb
Spanx <89501533+Lixeer@users.noreply.github.com>
2023-10-30 21:33:30 +0800

    Update protocol.py

--- libs/middleware/protocol.py
+++ libs/middleware/protocol.py
@@ -1,1 +1,28 @@
 #coding:utf-8
+
+from libs.netpackage.postpackage import AbsPostPackage
+from libs.singleton import Singleton
+
+class MiddleWareControl():
+	class _DefaultMiddleWare:
+		def verify(self,netpackage):
+			return True
+	pluginsMiddleWareMap=dict()
+	globaMiddleWare = AbsPostPackage
+
+
+
+class _MiddleWare:
+  def verify(self,netpackage : AbsPostPackage):
+		raise NotApproachmentException()
+
+class GlobalMiddleWare(_MiddleWare,Singleton):
+	pass
+
+class PluginMiddleWare(_MiddleWare):
+	pass
+
+def globalmiddleware():
+	def rg(func):
+		
+	
